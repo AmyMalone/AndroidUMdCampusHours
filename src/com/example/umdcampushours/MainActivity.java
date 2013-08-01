f3c7e6f8dd0dc25df1da439528beec46be0872ff
Amy Malone <amalone2@terpmail.umd.edu>
2013-08-01 01:10:38 -0400

    Added maker with title and hours to 251

--- src/com/example/umdcampushours/MainActivity.java
+++ src/com/example/umdcampushours/MainActivity.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
 public class MainActivity extends Activity {
+	private GoogleMap mMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setUpMapIfNeeded();
 	}
 
 	@Override
@@ -18,5 +28,23 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void setUpMapIfNeeded() {//add to onResume()
+	    // Do a null check to confirm that we have not already instantiated the map.
+	    if (mMap == null) {
+	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+	                            .getMap();
+	        Marker two51North = mMap.addMarker(new MarkerOptions()
+	        .position(new LatLng(38.992668,  -76.949722))
+	        .title("251")
+	        .snippet("Monday-Friday 5:30-8:30"));	
+	        // Check if we were successful in obtaining the map.
+	        if (mMap != null) {
+	            // The Map is verified. It is now safe to manipulate the map.
+
+	        }
+	    }
+	}
+	
 
 }
